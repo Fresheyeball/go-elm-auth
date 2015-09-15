cf3ea4e633b086917d931ceea98346950add7123
Isaac Shapira <isaac.shapira@logrhythm.com>
2015-09-15 17:19:56 -0600

    ignorance

--- main.go
+++ main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	SYS "syscall"
+
+	"github.com/googollee/go-socket.io"
+	"github.com/vrecan/death"
 )
 
+func connection(socket socketio.Socket) {
+	log.Println("Connected!")
+}
+
 func main() {
+	sockets, _ := socketio.NewServer(nil)
+	sockets.On("connection", connection)
 	log.Println("Listening at http://localhost:8000")
-	panic(http.ListenAndServe(":8000", http.FileServer(http.Dir("ui/dist"))))
+	http.ListenAndServe(":8000", http.FileServer(http.Dir("ui/dist")))
+	death.NewDeath(SYS.SIGINT, SYS.SIGTERM).WaitForDeath()
 }
