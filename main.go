6b38b0a4aa322196b93141d06b32338aa4a2be10
Isaac Shapira <fresheyeball@gmail.com>
2015-09-15 23:04:05 -0600

    socket hello world with factorial

--- main.go
+++ main.go
@@ -3,19 +3,44 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 	SYS "syscall"
 
 	"github.com/googollee/go-socket.io"
 	"github.com/vrecan/death"
 )
 
+const (
+	messageKey = "message"
+	roomKey    = "room"
+)
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
 func connection(socket socketio.Socket) {
 	log.Println("Connected!")
+	socket.Join(roomKey)
+}
+
+func message(sockets *socketio.Server) func(string) {
+	return func(message string) {
+		log.Println(message)
+		sockets.BroadcastTo(
+			roomKey,
+			messageKey,
+			strconv.Itoa(factorial(len(message))))
+	}
 }
 
 func newSocket() *socketio.Server {
 	sockets := attemptGet(socketio.NewServer(nil)).(*socketio.Server)
 	attempt(sockets.On("connection", connection))
+	attempt(sockets.On(messageKey, message(sockets)))
 	return sockets
 }
 
--- ui/src/Main.elm
+++ ui/src/Main.elm
@@ -3,30 +3,47 @@ module Main where
 import Html exposing (..)
 import Html.Events as Events
 import SocketIO exposing (..)
-import Task exposing (Task)
+import Task exposing (Task, andThen)
 import Signal exposing (..)
+
+messageKey : String
+messageKey = "message"
 
 socket : Task x Socket
 socket =
-  io "http://localhost:8000/socket" SocketIO.defaultOptions
+  io "http://localhost:8000/socket" defaultOptions
 
 onConnect : Mailbox Bool
 onConnect = mailbox False
 
 port connect : Task x ()
-port connect = socket `Task.andThen` connected onConnect.address
+port connect =
+  socket `andThen` connected onConnect.address
 
 onInput : Mailbox String
 onInput = mailbox ""
 
-view : Bool -> Html
-view status = let
+port send : Signal (Task x ())
+port send =
+  andThen socket << emit messageKey <~ onInput.signal
+
+onMessage : Mailbox String
+onMessage = mailbox ""
+
+port recieve : Task x ()
+port recieve =
+  andThen socket <| on messageKey onMessage.address
+
+view : Bool -> String -> Html
+view status response = let
   status' = case status of
     True -> "Connected!"
     False -> "Not connected :("
   in div []
   [ h2 [] [ text status' ]
-  , input [ Events.on "input" Events.targetValue (Signal.message onInput.address) ] [] ]
+  , input [ Events.on "input" Events.targetValue (Signal.message onInput.address) ] []
+  , br [] []
+  , text ("factorial of the message length is " ++ response) ]
 
 main : Signal Html
-main = view <~ onConnect.signal
+main = view <~ onConnect.signal ~ onMessage.signal
